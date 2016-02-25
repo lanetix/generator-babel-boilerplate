1cd502f7f23987a2ce50835281d2b0aae6e52978
James Wyatt Cready <wyatt.cready@lanetix.com>
2016-02-25 15:48:29 -0500

    Shave some bytes

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,9 +1,9 @@
 import API from '../lib/api'
 
-export function handler (event, context) {
+export function handler (event, { succeed, fail }) {
   const { contents: { recordId, recordType, toStage } } = event
   const request = API(event)
-  const done = (e, res) => e ? context.fail(e) : context.succeed(res)
+  const done = (e, res) => e ? fail(e) : succeed(res)
   const chance_to_win = ({
     third: 0.25,
     forth: 0.50,
